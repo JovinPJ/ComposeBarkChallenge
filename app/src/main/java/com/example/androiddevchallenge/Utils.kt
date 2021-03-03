63dc2598746dcb2ff3827182393413305d707b0d
Christy <christy.james@revinate.com>
2021-03-03 16:45:38 +0530

    UI Changes and Dummy data for Puppy app

---
+++ app/src/main/java/com/example/androiddevchallenge/Utils.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.androiddevchallenge
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringArrayResource
+import com.example.androiddevchallenge.model.Puppy
+
+object Utils {
+    private val images = arrayListOf(
+        R.drawable.puppyone,
+        R.drawable.puppytwo,
+        R.drawable.puppythree,
+        R.drawable.puppyfour,
+        R.drawable.puppyfive,
+        R.drawable.puppysix,
+        R.drawable.puppyseven,
+        R.drawable.puppyeight,
+        R.drawable.puppynine,
+    )
+
+    @Composable
+    fun getPuppies(): MutableList<Puppy> {
+        val puppies: MutableList<Puppy> = mutableListOf()
+        val puppyBreed = stringArrayResource(R.array.breed)
+        val puppyDesc = stringArrayResource(R.array.desc)
+        for (i in puppyBreed.indices) {
+            puppies.add(Puppy(puppyBreed[i], images[i], puppyDesc[i]))
+        }
+        return puppies
+    }
+
+    @Composable
+    fun getPuppy(pos: Int): Puppy {
+        val puppyBreed = stringArrayResource(R.array.breed)
+        val puppyDesc = stringArrayResource(R.array.desc)
+        return Puppy(puppyBreed[pos], images[pos], puppyDesc[pos])
+    }
+}
